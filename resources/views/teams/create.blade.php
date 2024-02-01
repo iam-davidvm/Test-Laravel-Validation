df9447c5c726e8ab37b56f2ea3624f1adc2e930a
iam-davidvm <vanmelkebekedavid@gmail.com>
2024-02-01 20:58:55 +0100

    locally all test run succesfully

--- resources/views/teams/create.blade.php
+++ resources/views/teams/create.blade.php
@@ -3,7 +3,7 @@
     Name:
     <br />
     {{-- TASK: change this field so it would contain old value after validation error --}}
-    <input type="text" name="name" />
+    <input type="text" name="name" vale="{{ old('name') }}" />
     <br /><br />
     <button type="submit">Save</button>
 </form>
